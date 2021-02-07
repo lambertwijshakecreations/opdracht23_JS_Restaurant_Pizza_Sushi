31cdfe8c8c56bb1bebcd152d920e01603448e836
lambertwijshakecreations <73013216+lambertwijshakecreations@users.noreply.github.com>
2021-02-07 11:35:09 +0100

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,24 @@
+const hawaiPizza = function(){
+    console.log("Maak deeg.");
+    console.log("Maak saus.");
+    console.log("Snij vlees.");
+    console.log("Maak pizza bodem.");
+    console.log("Bedek bodem met saus.");
+    console.log("Strooi vlees en annanas op de saus.");
+    console.log("Bak de pizza.");
+}
+
+const sushiRoll = function(){
+    console.log("Maak rijst.");
+    console.log("Snij zeewier.");
+    console.log("Snij vis.");
+    console.log("Doe vis in rijst.");
+    console.log("Rol een sushi.");
+    console.log("Snij in stukken.");
+}
+
+sushiRoll();
+sushiRoll();
+sushiRoll();
+hawaiPizza();
+hawaiPizza();
